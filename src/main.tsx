8409176968ce32b7f178a6b26d7b87dcb5d97c82
jamurkampus <jamurkampus@gmail.com>
2025-04-22 10:58:18 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import OverrideAgent from './OverrideAgent'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Landing from './Landing';
+import OverrideAgent from './OverrideAgent';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <OverrideAgent />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/agent" element={<OverrideAgent />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
