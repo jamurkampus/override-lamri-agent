30f570753e9573d63fd2a33631c6ff6e5e759b78
jamurkampus <jamurkampus@gmail.com>
2025-04-22 12:53:26 +0800

    Update Landing.tsx

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -3,10 +3,8 @@ import { useNavigate } from "react-router-dom";
 
 export default function Landing() {
   const navigate = useNavigate();
-
   useEffect(() => {
     navigate("/dashboard", { replace: true });
   }, []);
-
   return null;
 }
