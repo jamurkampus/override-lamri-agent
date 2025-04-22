fefa06ca14136bcd1e91c3622f3ddc6d1b978499
jamurkampus <jamurkampus@gmail.com>
2025-04-22 15:56:14 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-// src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Landing from './Landing';
 import OverrideAgent from './OverrideAgent';
 import Dashboard from './Dashboard';
+import Portfolio from './pages/portfolio'; // << Tambahkan ini
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -13,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<Landing />} />
         <Route path="/agent" element={<OverrideAgent />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/portfolio" element={<Portfolio />} /> {/* Tambahkan ini */}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
