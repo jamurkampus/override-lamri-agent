788f626440bc32c59e1580ae68abb9d1b7787f65
jamurkampus <jamurkampus@gmail.com>
2025-04-22 12:54:12 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Landing from './Landing';
-import OverrideAgent from './OverrideAgent';
 import Dashboard from './Dashboard';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -10,7 +9,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/agent" element={<OverrideAgent />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
